f365a283905dfd436c95d7b28a207bc3812ee2b4
Steffen Bartsch <sbartsch@tzi.org>
2009-02-02 15:10:22 +0100

    Example migration when using model security

---
+++ db/migrate/20090112084912_add_first_user.rb
@@ -0,0 +1,19 @@
+require "vendor/plugins/declarative_authorization/lib/maintenance"
+
+class AddFirstUser < ActiveRecord::Migration
+  def self.up
+    Authorization::Maintenance::without_access_control do
+      u = User.create(:login => 'first', :email => 'bla@bla.de',
+        :password => 'blabla', :password_confirmation => 'blabla')
+      p u
+      u.save!
+    end
+  end
+
+  def self.down
+    Authorization::Maintenance::without_access_control do
+      u = User.find_by_login("first")
+      u.destroy if u
+    end
+  end
+end
