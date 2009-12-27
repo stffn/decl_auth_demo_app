422ba520c719504a1de1086bea9cfdc29750d6d2
steffen <sbartsch@tzi.org>
2009-12-27 17:31:42 +0100

    simplified require for decl_auth test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,7 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
 # For declarative_authorization test helpers
-require File.expand_path(File.dirname(__FILE__) +
-    "/../vendor/plugins/declarative_authorization/lib/declarative_authorization/maintenance")
+require "declarative_authorization/maintenance"
 
 class ActiveSupport::TestCase
   # We need to include the declarative_authorization helpers
